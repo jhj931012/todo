0cfb4e85c3019ecd02d0f33976561eb2de4d2318
jhj <jungheejae931012@gmail.com>
2023-01-18 14:35:38 +0900

    todo entity 설계

---
+++ src/main/java/com/example/todo/todoapi/entity/TodoEntity.java
@@ -0,0 +1,33 @@
+package com.example.todo.todoapi.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// 일정 관리 프로그램
+@Setter @Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "todoId")
+@Builder
+@Entity
+@Table(name = "tbl_todo")
+public class TodoEntity {
+
+    // 
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false, length = 30)
+    private String title; // 제목
+        
+    private boolean done; // 일정 완료 여부
+
+    @CreationTimestamp
+    private LocalDateTime createDate; // 등록 시간
+}
